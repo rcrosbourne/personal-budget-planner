faacc229c5e291e12caafd1292f631bfd3a17644
rcrosbourne <rcrosbourne@gmail.com>
2023-02-19 05:26:47 -0500

    Cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-
 import React from "react";
 import { Menu } from "react-feather";
 import AppScreenshot from "../components/AppScreenshot";
